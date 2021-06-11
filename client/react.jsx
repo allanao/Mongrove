421d0042b6b92f56c7ec03f2f216f00c8e1d0498
Allana Ordonez <ayaordonez@gmail.com>
2021-06-10 17:02:05 -0700

    test

--- client/react.jsx
+++ client/react.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const Index = () => {
-    return <div>Hello React!!!</div>;
+    return <div>Hello React!!! I am being watched</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById('app'));
