a6263e337651aa451da83fda1de9cce6fef4c3c2
Allana Ordonez <ayaordonez@gmail.com>
2021-06-11 20:22:06 -0700

    test commit

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -8,21 +8,21 @@ const Navbar = () => {
   
   return (
     <HashRouter>
-    <div className="Navbar">
-        <div className="menu">
-           <h1>Mongrove</h1>
-            {/* view button - switch between landing page and query page */}
-            <Link to="/landingPage"><img src="assets/images/icon1"/> </Link>
-            <Link to="/queryPage"><img src="assets/images/icon2"/> </Link>
-            {/* help button - launch help window  */}
+        <div className="Navbar">
+            <div className="menu">
+            <h1>Mongrove</h1>
+                {/* view button - switch between landing page and query page */}
+                <Link to="/landingPage"><img src="assets/images/icon1"/> </Link>
+                <Link to="/queryPage"><img src="assets/images/icon2"/> </Link>
+                {/* help button - launch help window  */}
+            </div>
+            <Switch>
+                <Route exact path="/landingPage" component={LandingPage}/>
+                <Route exact path="/queryPage"  component={QueryPage}/>
+            </Switch>
         </div>
-        <Switch>
-            <Route exact path="/landingPage" component={LandingPage}/>
-            <Route exact path="/queryPage"  component={QueryPage}/>
-        </Switch>
-    </div>
+    </HashRouter>
   )
-  </HashRouter>
 };
 
 
